941892e9a372ef72ecd6018c6ff98c01abad4dca
Lachlan Casey <lachlan.612@gmail.com>
2023-01-24 13:41:31 +1000

    deatime plots now iterate detectors correctly

--- xfmreadout/dtops.py
+++ xfmreadout/dtops.py
@@ -19,40 +19,36 @@ def dthist(dt, odir, ndet):
     ax.legend(loc=1, title="Detector:")
 
     fig.savefig(os.path.join(odir, 'deadtime_histograms.png'), dpi=150)
-    fig.show()
     return
 
 def dtimages(dt, odir, xres, yres, ndet):
-    fig = plt.figure(figsize=(8,4))
+    fig, ax = plt.subplots(1, 2, figsize=(8,4))
 
-    ax0 = fig.add_subplot(121)
-    ax1 = fig.add_subplot(122)
+    cset = ['red', 'blue']
 
-    ax0.set_title("Detector: 0")
-    ax0.tick_params(axis='x',colors='red')
-    ax0.tick_params(axis='y',colors='red')
-    for spine in ax0.spines.values():
-        spine.set_linewidth(2)
-        spine.set_color('red')
+    for i in np.arange(ndet):
 
-    ax1.set_title("Detector: 1")
-    ax1.tick_params(axis='x',colors='blue')
-    ax1.tick_params(axis='y',colors='blue')
-    for spine in ax1.spines.values():
-        spine.set_linewidth(2)
-        spine.set_color('blue')
+        ax[i].set_title(f"Detector: {i}")
+        ax[i].tick_params(axis='x',colors=cset[i])
+        ax[i].tick_params(axis='y',colors=cset[i])
+        for spine in ax[i].spines.values():
+            spine.set_linewidth(2)
+            spine.set_color(cset[i])
 
-    dtimage0 = dt[0].reshape(yres,xres)
-    dtimage1 = dt[1].reshape(yres,xres)
+        dtimage = dt[i].reshape(yres,xres)
 
-    ax0.imshow(dtimage0, cmap="magma")
-    ax1.imshow(dtimage1, cmap="magma")
+        ax[i].imshow(dtimage, cmap="magma")
 
     fig.savefig(os.path.join(odir, 'deadtime_maps.png'), dpi=150)
-    fig.show()
     return
 
 def diffimage(sum, odir, xres, yres, ndet):
+    
+    if ndet != 2:
+        raise ValueError("Number of detectors != 2, difference map not possible")
+       
+    sum=sum.astype(float)
+         
     diffmap = sum[0]-sum[1]
 
     diffimage = diffmap.reshape(yres,xres)
@@ -66,7 +62,6 @@ def diffimage(sum, odir, xres, yres, ndet):
     plt.colorbar(img, fraction=0.04346, pad=0.04)
 
     plt.savefig(os.path.join(odir, 'difference_map.png'), dpi=150)
-    plt.show()
     return
 
 def dtscatter(dt, sum, odir, ndet):
@@ -76,8 +71,8 @@ def dtscatter(dt, sum, odir, ndet):
     ax.set_xlabel("Deadtime (%)")
     ax.set_ylabel("Counts")
 
-    ax.scatter(dt[0],sum[0], color="red", marker='o', s=50, alpha=0.1, linewidths=None, label="0")
-    ax.scatter(dt[1],sum[0], color="blue", marker='o', s=50, alpha=0.1, linewidths=None, label="1")
+    for i in np.arange(ndet):
+        ax.scatter(dt[i],sum[i], color=cset[i], marker='o', s=50, alpha=0.1, linewidths=None, label=f"{i}")
 
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
@@ -87,7 +82,7 @@ def dtscatter(dt, sum, odir, ndet):
     #from: https://stackoverflow.com/questions/4700614/how-to-put-the-legend-outside-the-plot
 
     fig.savefig(os.path.join(odir, 'deadtime_vs_counts.png'), dpi=150)
-    fig.show()
+    return
 
 def dtplots(config, odir, dt, sum, xres, yres, ndet):
 
@@ -98,6 +93,8 @@ def dtplots(config, odir, dt, sum, xres, yres, ndet):
         diffimage(sum, odir, xres, yres, ndet)
         dtscatter(dt, sum, odir, ndet)
     elif (np.amax(sum) <= 0):
-        raise ValueError("Sum array zero/not present, cannot generate sum plots")
+        raise ValueError("Sum array is empty or zero - cannot generate sum plots")
+
+    plt.close()
 
     return 
