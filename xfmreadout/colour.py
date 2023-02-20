b8ea2606d07d5d405b9ebb24a2c3ec16652b0931
lachlanc61 <lachlanc.612@gmail.com>
2023-02-20 22:27:39 +1000

    colour map now using cyclic hsv from lo to elastic_Ka

--- xfmreadout/colour.py
+++ xfmreadout/colour.py
@@ -6,10 +6,9 @@ import sys
 #from colorsys import hsv_to_rgb
 
 import xfmreadout.utils as utils
+import matplotlib
 import matplotlib.pyplot as plt
-
 from matplotlib import cm
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 #-----------------------------------
 #MODIFIABLE CONSTANTS
@@ -25,64 +24,80 @@ from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 #   https://stackoverflow.com/questions/1977362/how-to-create-module-wide-variables-in-python
 this = sys.modules[__name__]
 
+custom_colour_dict = \
+        {'red':   [[0.0,  1.0, 1.0],
+                   [0.143,  1.0, 1.0],
+                   [0.286,  1.0, 1.0],
+                   [0.429,  0.0, 0.0],
+                   [0.571,  0.0, 0.0],
+                   [0.714,  0.0, 0.0],
+                   [0.857,  1.0, 1.0],
+                   [1.0,  1.0, 1.0]],
+
+         'green': [[0.0,  0.0, 0.0],
+                   [0.143,  0.0, 0.0],
+                   [0.286,  1.0, 1.0],
+                   [0.429,  1.0, 1.0],
+                   [0.571,  1.0, 1.0],
+                   [0.714,  0.0, 0.0],
+                   [0.857,  0.0, 0.0],
+                   [1.0,  1.0, 1.0]],
+                   
+         'blue':  [[0.0,  1.0, 1.0],
+                   [0.143,  0.0, 0.0],
+                   [0.286,  0.0, 0.0],
+                   [0.429,  0.0, 0.0],
+                   [0.571,  1.0, 1.0],
+                   [0.714,  1.0, 1.0],
+                   [0.857,  1.0, 1.0],
+                   [1.0,  1.0, 1.0]]}
+
+
+
 #-----------------------------------
 #FUNCTIONS
 #-----------------------------------
 
-def initialise(config, e):
+def initialise(config, energy):
     """
-    initialise the colour gaussians 
-    export to module-wide variables via "this"
+    initialise the colourmap
 
     receives energy channel list
-    returns None
-
-    
-    NB: not certain this is optimal - just need to get e somehow
-        could also create in parallel via config.NCHAN & ESTEP
+    returns red, green, blue arrays
     """
-    kachan=utils.lookfor(e,float(config['ELASTIC']+config['EOFFSET']))  #K-alpha channel
-    npad=config['NCHAN']-kachan
+    
+    chan_lo=100
+    chan_hi=np.where(energy==config['ELASTIC'])[0][0]
 
-    hsv=cm.get_cmap('hsv', kachan)
-    cmap=hsv(range(kachan))
+    cmap = matplotlib.colors.LinearSegmentedColormap('testCmap', segmentdata=custom_colour_dict, N=chan_hi)
 
-    cred=cmap[:,0]
-    cgreen=cmap[:,1]
-    cblue=cmap[:,2]
+    rgba = cmap(np.linspace(0, 1, chan_hi-chan_lo))
 
-    cred=np.pad(cred, (0, npad), 'constant', constant_values=(0, 1))
-    cgreen=np.pad(cgreen, (0, npad), 'constant', constant_values=(0, 0))
-    cblue=np.pad(cblue, (0, npad), 'constant', constant_values=(0, 0))    
+    red=rgba[:,0]
+    green=rgba[:,1]
+    blue=rgba[:,2]
 
-    #assign to module-wide variables
-    this.red=cred
-    this.green=cgreen
-    this.blue=cblue
+    pad_hi=len(energy)-chan_hi
+    pad_lo=chan_lo
 
-    return None
+    red=np.pad(red, (pad_lo, pad_hi), mode='constant', constant_values=(1, 1))
+    green=np.pad(green, (pad_lo, pad_hi), mode='constant', constant_values=(0, 1))
+    blue=np.pad(blue, (pad_lo, pad_hi), mode='constant', constant_values=(1, 1))    
 
-def spectorgb(config, e, y):
+    return red, green, blue
+
+def spectorgb(energy, spectrum, red, green, blue):
     """
     maps spectrum onto R G B channels 
-    use HSV colourmap to generate
+    use RGBA colourmap to generate
     """
-    #bg = pybaselines.smooth.snip(y,SNIPWINDOW)[0]
-    #y=y-bg
-
-    #if doing log y
-    if config['RGBLOG']:
-        #convert y to float for log
-        yf=y.astype(float)
-        #log y, excluding 0 values
-        y=np.log(yf, out=np.zeros_like(yf), where=(yf!=0))
 
     #multiply y vectorwise onto channels (t/px: 0.004051 s)
-    rsum=np.sum(y*(this.red))/len(e)
-    gsum=np.sum(y*(this.green))/len(e)
-    bsum=np.sum(y*(this.blue))/len(e)
+    rsum=np.sum(spectrum*(red))/len(energy)
+    gsum=np.sum(spectrum*(green))/len(energy)
+    bsum=np.sum(spectrum*(blue))/len(energy)
 
-    ysum=np.sum(y)
+    ysum=np.sum(spectrum)
     
 #    max=np.max([rsum,bsum,gsum])
 
@@ -110,25 +125,60 @@ def compile(rvals, gvals, bvals, mapx, mapy):
     gimg=np.reshape(gvals, (-1, mapx))
     bimg=np.reshape(bvals, (-1, mapx))
 
-    rgbarray = np.zeros((mapy,mapx,3), 'uint8')
-    rgbarray[..., 0] = rimg*256
-    rgbarray[..., 1] = gimg*256
-    rgbarray[..., 2] = bimg*256
+    rgbimg = np.zeros((mapy,mapx,3), 'uint8')
+    rgbimg[..., 0] = rimg*256
+    rgbimg[..., 1] = gimg*256
+    rgbimg[..., 2] = bimg*256
     
-    return(rgbarray, rvals, gvals, bvals)
+    return(rgbimg, rvals, gvals, bvals)
 
-def export_show(rgbarray, rvals, gvals, bvals, dirs):
 
-    np.savetxt(os.path.join(dirs.transforms, "colourmap_red.txt"), rvals)
-    np.savetxt(os.path.join(dirs.transforms, "colourmap_green.txt"), gvals)
-    np.savetxt(os.path.join(dirs.transforms, "colourmap_blue.txt"), bvals)
+def plot_colourmap_explainer(energy, spectrum, red, green, blue, dirs):
+    """
+    displays a single spectrum coloured by final map and overlaid with R G B channel multipliers
+    """
+    fig, ax1 = plt.subplots() 
 
-    plt.imshow(rgbarray)
+    ax1.set_xlabel('energy (kV)') 
+    ax1.set_ylabel('Intensity') 
+    ax1.axis(xmin=0,xmax=30)
+    ax1.set_yscale('log')
+    #ax1.axis(ymin=vymin,ymax=1)
+    ax1.axis(ymin=0.00005,ymax=1)
+
+    ax2 = ax1.twinx() 
+    ax2.set_ylabel('RGB')
+    ax2.axis(ymin=0,ymax=1.05)
+
+    ax2.plot(energy, red, '#ff0000', linestyle='dashed', linewidth=1, label="R")
+    ax2.plot(energy, green, '#00ff00', linestyle='dashed', linewidth=1, label="G")
+    ax2.plot(energy, blue, '#0000ff', linestyle='dashed', linewidth=1, label="B")
+
+    ax2.legend(loc=(0.85,0.7))
+
+    ax1.plot(energy, spectrum, color="gray")
+    for i in range(len(spectrum) - 1):
+        ax1.fill_between([energy[i], energy[i+1]], [spectrum[i], spectrum[i+1]], y2=-1, color=(red[i],green[i],blue[i]))
+
+
+    plt.savefig(os.path.join(dirs.plots, 'rgba_spectrum.png'), dpi=150)
+    plt.show()
+
+def export_show(rgbimg, rvals, gvals, bvals, dirs):
+    """
+    saves colourmap and displays image
+    """
+
+    np.savetxt(os.path.join(dirs.transforms, "colourmap_red.txt"), rvals, delimiter=',')
+    np.savetxt(os.path.join(dirs.transforms, "colourmap_green.txt"), gvals, delimiter=',')
+    np.savetxt(os.path.join(dirs.transforms, "colourmap_blue.txt"), bvals, delimiter=',')
+
+    plt.imshow(rgbimg)
     plt.savefig(os.path.join(dirs.plots, 'colours.png'), dpi=150)
 
 
 def calccolours(config, pixelseries, xfmap, dataset, dirs):
-        initialise(config, xfmap.energy)
+        red, green, blue = initialise(config, pixelseries.energy)
         
         rvals=np.zeros(pixelseries.npx)
         gvals=np.zeros(pixelseries.npx)
@@ -137,10 +187,11 @@ def calccolours(config, pixelseries, xfmap, dataset, dirs):
 
         for i in np.arange(pixelseries.npx):
             counts=dataset[i,:]
-            rvals[i], bvals[i], gvals[i], totalcounts[i] = spectorgb(config, xfmap.energy, counts)
+            rvals[i], bvals[i], gvals[i], totalcounts[i] = spectorgb(pixelseries.energy, counts, red, green, blue)
 
-        rgbarray, rvals, gvals, bvals = compile(rvals, gvals, bvals, xfmap.xres, pixelseries.nrows)
+        rgbimg, rvals, gvals, bvals = compile(rvals, gvals, bvals, xfmap.xres, pixelseries.nrows)
 
-        export_show(rgbarray, rvals, gvals, bvals, dirs)
+        export_show(rgbimg, rvals, gvals, bvals, dirs)
 
-        return rgbarray, rvals, gvals, bvals
+        
+        return rgbimg, rvals, gvals, bvals
