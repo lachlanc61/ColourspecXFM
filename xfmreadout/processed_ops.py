536398b0c7160cb1553078070356013c2feb5374
lachlanc61 <lachlanc.612@gmail.com>
2023-04-02 15:59:10 +1000

    processed data routines now in processed_ops, HDBSCAN working

--- xfmreadout/processed_ops.py
+++ xfmreadout/processed_ops.py
@@ -1,22 +1,25 @@
 import os
 import re
-import sys
 import numpy as np
 import periodictable as pt
-import matplotlib.pyplot as plt
+import pandas as pd
 from PIL import Image
 
 import xfmreadout.clustering as clustering
+import xfmreadout.processed_plots as processed_plots
 
 
-IGNORE_ELEMENTS=['sum','Compton', 'Mo']
+FORCE = False
+AUTOSAVE = True
+
+EMBED_DIRNAME = "embedding"
+
+IGNORE_ELEMENTS=['sum','Back','Compton','Mo','MoL']
 
 TRUE_ELEMENTS = []
 
 for ptelement in pt.elements:
     TRUE_ELEMENTS.append(ptelement.symbol)
-
-
 
 
 def get_elements(files):
@@ -60,55 +63,130 @@ def get_elements(files):
     return elements, files
 
 
-
-
-def modify_maps()
+def modify_maps(maps, elements):
     BASEFACTOR=100000
     MODIFY_LIST = ['Na', 'Mg', 'Al', 'Si']
     MODIFY_FACTORS = [ 100, 1, 1, 1 ]
 
-
-
-
-def main(image_directory):
-
-    files = [f for f in os.listdir(wdir) if f.endswith('.tiff')]
-
-    elements, files = get_elements(files)
-
-    filepaths = [os.path.join(wdir, file) for file in files ] 
-
-    #get file dimensions:
-
-    im = Image.open(os.path.join(wdir, filepaths[0]))
-    img = np.array(im)
-
-    dims = img.shape
-
-    maps=np.zeros((len(elements), dims[0], dims[1]), dtype=np.float32)
-
-
     i=0
-    for f in filepaths:
-        im = Image.open(f)
-        img = np.array(im)
-        #replace all negative values with 0
-        img = np.where(img<0, 0, img)
-
+    for i in range(maps.shape[0]):
         factor=BASEFACTOR
+
         for idx, snames in enumerate(MODIFY_LIST):
             if elements[i] in snames:
                 factor=BASEFACTOR*MODIFY_FACTORS[idx]
 
-        maps[i,:,:]=img/factor
+        maps[i,:,:]=maps[i,:,:]/factor
         i+=1
-        
+
+    return maps
+
+
+def load_maps(filepaths):
+
+    #load an image and check dimensions
+    im = Image.open(filepaths[0])
+    img = np.array(im)
+
+    dims = img.shape
+
+    maps=np.zeros((len(filepaths), dims[0], dims[1]), dtype=np.float32)
+
+    i=0
+    for f in filepaths:
+            im = Image.open(f)
+            img = np.array(im)
+            #replace all negative values with 0
+            img = np.where(img<0, 0, img)
+            if not (img.shape == dims):
+                raise ValueError(f"unexpected dimensions for file {f}")
+            maps[i,:,:]=img
+            i+=1
+    
+    return maps
+
+
+def get_embedding(data, mapshape, image_directory):
+    """
+    calculate embedding
+    """
+
+    N_CLUSTERS=6
+
+    EMBED_DIR=os.path.join(image_directory,EMBED_DIRNAME)
+
+    if not os.path.exists(EMBED_DIR):
+        os.mkdir(EMBED_DIR)
+
+    NPX=mapshape[1]*mapshape[2]
+
+    NCHAN=mapshape[0]
+
+    file_cats=os.path.join(EMBED_DIR,"categories.npy")
+    file_classes=os.path.join(EMBED_DIR,"classavg.npy")
+    file_embed=os.path.join(EMBED_DIR,"embedding.npy")
+    file_ctime=os.path.join(EMBED_DIR,"clusttimes.npy")
+
+    filesexist = os.path.isfile(file_cats) and os.path.isfile(file_classes) \
+        and  os.path.isfile(file_embed) and os.path.isfile(file_ctime)
+
+    if FORCE or not filesexist:
+        categories, classavg, embedding, clusttimes = clustering.calculate(data, NPX, N_CLUSTERS, NCHAN )
+        #embedding, clusttimes = clustering.reduce(data)
+        if AUTOSAVE:
+            np.save(file_cats,categories)
+            np.save(file_classes,classavg)
+            np.save(file_embed,embedding)
+            np.save(file_ctime,clusttimes)
+    else:
+        categories = np.load(file_cats)
+        classavg = np.load(file_classes)
+        embedding = np.load(file_embed)
+        clusttimes = np.load(file_ctime)
+
+    return categories, classavg, embedding, clusttimes
+
+def plot_all(categories, classavg, embedding, maps, elements):
+
+    IDX=5       #element index
+    REDUCER=1   #reducer to use
+
+    processed_plots.show_map(maps, elements, IDX)
+
+    processed_plots.category_map(categories, maps)
+    
+    processed_plots.category_avgs(categories, elements, classavg)
+
+
+    df= pd.DataFrame(embedding[REDUCER,:], columns=["x","y"])
+
+    df["cat"]=categories[REDUCER,:]
+
+    processed_plots.seaborn_embedplot(df)
+    processed_plots.seaborn_kdeplot(df)
+
+
+def main(image_directory):
+
+    files = [f for f in os.listdir(image_directory) if f.endswith('.tiff')]
+
+    elements, files = get_elements(files)
+
+    filepaths = [os.path.join(image_directory, file) for file in files ] 
+
+    maps = load_maps(filepaths)
+
+    maps = modify_maps(maps, elements)
 
     data=maps.reshape(maps.shape[0],-1)
 
     data=np.swapaxes(data,0,1)
 
-    print(maps.shape, data.shape)
-    
+    #print(maps.shape, data.shape)
 
+    categories, classavg, embedding, clusttimes = get_embedding(data, maps.shape, image_directory)
+
+    plot_all(categories, classavg, embedding, maps, elements)
+
+    return categories, classavg, embedding, clusttimes, maps, elements
 
---
+++ xfmreadout/processed_plots.py
@@ -0,0 +1,149 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import seaborn as sns
+
+REDUCER=1
+
+def show_map(maps, elements, idx):
+
+    img = maps[idx,:,:]
+
+    print(elements[idx])
+    print(np.quantile(img, 0.1), np.quantile(img, 0.5), np.quantile(img, 0.9))
+
+    fig = plt.figure(figsize=(12,6))
+
+    ax = fig.add_subplot(111)
+
+    display = ax.imshow(img, cmap='plasma')
+
+    plt.show()
+
+    return
+
+def category_map ( categories, maps ):
+    """
+    image of categories
+    """
+
+    from matplotlib import cm
+
+    KCMAPS=["tab10","tab10"]    #colourmaps for kmeans
+
+    categories.shape
+
+    fig = plt.figure(figsize=(12,6))
+    ax = fig.add_subplot(111)
+
+    ncats=np.max(categories)+1
+
+    axcm=cm.get_cmap(KCMAPS[REDUCER], ncats)
+
+    cmap=axcm(range(ncats))
+
+    #reshape the category list back to the map dimensions using xdim
+    #WARNING: fails using SHORTRUN unless ends at end of row - fix this later
+
+    catmap=np.reshape(categories[REDUCER], [maps.shape[1],-1])
+
+    #show this category image
+    ax.imshow(catmap, cmap=KCMAPS[REDUCER])
+
+    return
+
+    #fig.savefig(os.path.join(EMBED_DIR,"cluster_map.png"), dpi=200)
+
+def category_avgs(categories, elements, classavg):
+    fig = plt.figure(figsize=(12,6))
+    ax = fig.add_subplot(111)
+
+    ncats=np.max(categories)+1
+
+    elids=range(len(elements))
+
+    #ax.set_yscale('log')
+
+    for i in range(ncats-1):
+        ax.plot(elements, classavg[0,i,:], linewidth=1)
+
+
+    fig.show()
+
+    return
+
+def category_boxplots(data, categories, elements):
+    """
+    category:element boxplots
+    """
+
+    ncats=np.max(categories)+1
+    catlist=range(ncats)
+
+    print(categories.shape)
+    print(data.shape)
+
+    boxdata=np.zeros((ncats,len(elements),categories.shape[1]))
+
+
+    #fig = plt.figure(figsize =(12, 6))
+
+    fig, (ax) = plt.subplots(ncats, 1, figsize=(12, 24))
+
+    #ax = fig.add_subplot(111)
+
+    for cat_idx in catlist:
+        assigned=np.where(categories[REDUCER] == cat_idx, True, False)
+
+        selected=[]
+
+        for el_idx, ename in enumerate(elements):
+            boxdata[cat_idx,el_idx,:]=data[:,el_idx]*assigned
+            selected.append(data[assigned,el_idx])
+        
+            # Creating plot
+
+        #ax[cat_idx].set_yscale('log')
+
+        ax[cat_idx].boxplot(selected, labels=elements, whis=[0.01,99.99])
+
+    fig.show()
+
+def seaborn_embedplot(df):
+    ### scatter plot with marginal axes
+    sns.set_style('white')
+    sns.jointplot(x=df.x, y=df.y,
+                hue=df.cat, palette=sns.color_palette(),
+                lw=0,
+                height=10, ratio=6
+                )
+
+    #xlim=[-3,3], ylim=[-3,3],
+
+    ax = sns.despine(ax=None, left=True, bottom=True)
+    #plt.savefig('j_scatter_tr.png', transparent=True)
+    plt.show()
+
+    return
+
+def seaborn_kdeplot(df):
+    """
+    kde plot filled with colors with transparent background
+
+    """
+    sns.set_style('white')
+    ax = sns.jointplot(x=df.x, y=df.y,
+                cut = 0, hue=df.cat,
+                palette=sns.color_palette("dark"),
+                kind='kde', fill=True,
+                height=15, ratio=6,
+                joint_kws = dict(alpha=0.4),
+                marginal_kws=dict(fill=True),
+                legend=False)
+    ax.ax_marg_x.remove()
+    ax.ax_marg_y.remove()
+    ax = sns.despine(ax=None, left=True, bottom=True)
+    #plt.savefig('kde_tr_fill.png', transparent=True)
+    plt.show()
+
