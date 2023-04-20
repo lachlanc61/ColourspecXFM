5d8f8472ca8aa7aed1ff6fd8da5f57b6899343c2
lachlanc61 <lachlanc.612@gmail.com>
2023-04-20 20:37:45 +1000

    cleanup unused files

--- diag_trial.py
+++
@@ -1,8 +0,0 @@
-from xfmreadout import diagops as diagops
-
-
-#args = ["-f", "/mnt/d/DATA/XFMDATA/2023/Lachlan/REFERENCE/uMatter/Mo_vac_230313/50-200_TC1p0/diagnostics_um200.log",]
-
-args_in = ["-f", "/mnt/d/DATA/XFMDATA/2023/Lachlan/REFERENCE/uMatter/Mo_vac_230315/diagnostics.log", "-s"] 
-
-diagops.main(args_in)
