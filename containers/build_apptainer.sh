8b62a98a14f49a6b6750bf7b25c0ac85016f88a1
lachlanc61 <lachlanc.612@gmail.com>
2023-05-19 20:48:51 +1000

    correctly passing args to runscript

--- containers/build_apptainer.sh
+++ containers/build_apptainer.sh
@@ -5,6 +5,9 @@ IMAGENAME='xfmreadout_latest'
 
 apptainer build $SIFDIR/${IMAGENAME}.sif apptainer.def
 
+#test
+apptainer exec --containall --bind /tmp,$PWD/../data:/data  $SIFDIR/xfmreadout_latest.sif pytest /app
 
 #to run
-#apptainer exec --containall --bind /tmp,$PWD/data:/data  $SIFDIR/xfmreadout_latest.sif xfmread-raw -f /data/example_datafile.GeoPIXE -m
+#apptainer run --containall --bind /tmp,$PWD/data:/data  $SIFDIR/xfmreadout_latest.sif -f /data/example_datafile.GeoPIXE -m
+
